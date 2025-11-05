0a8bb18507b0e546d71036dcd905701d6344b6a1
fsologureng <fsologur@uchile.cl>
2025-11-05 16:45:24 -0300

    chore: update author in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 setuptools.setup(
     name="eol_jump_to",
     version="0.0.1",
-    author="matiassalinas",
-    author_email="matsalinas@uchile.cl",
+    author="EOL UChile",
+    author_email="eol-ing@uchile.cl",
     description="jump_to function version EOL",
     long_description="Redirect student to specific block if has access. Otherwise redirect to course index",
     url="https://eol.uchile.cl",
